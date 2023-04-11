3e1c276e34429bb6c3f9722043de7f1bce956403
Cassio Silva <Cassio Silva@192.168.1.38>
2023-04-10 23:42:24 -0300

    Public Class Renomeada

---
+++ faculdade_engenhariadesoftware/src/Questao_1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Questao_1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Digite o primeiro número: ");
+		float num1 = sc.nextFloat();
+
+		System.out.print("Digite o segundo número: ");
+		float num2 = sc.nextFloat();
+
+		float maiorNumero = num1;
+		if (num2 > num1) {
+			maiorNumero = num2;
+		}
+
+		System.out.println("O maior número é " + maiorNumero);
+		
+		sc.close();
+	}
+
+}

080e695c5252986a4001a8c77c22558a9835e9db
Cassio Silva <Cassio Silva@192.168.1.38>
2023-04-12 16:19:43 -0300

    Add Questão 2

--- faculdade_engenhariadesoftware/src/PositivoNegativo.java
+++ faculdade_engenhariadesoftware/src/PositivoNegativo.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
 
 public class PositivoNegativo {
 
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Digite um número: ");
+		float num = sc.nextFloat();
+
+		if (num > 0) {
+			System.out.printf("O número %.0f é POSITIVO", num);
+		} else if (num < 0) {
+			System.out.printf("O número %.0f é NEGATIVO", num);
+		} else {
+			System.out.println("O número 0 é NULO");
+		}
+
 	}
 
 }
